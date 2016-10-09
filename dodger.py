590e254130cf5152d4b9e578b9be6c87ac5ba04e
Tibqi <tibi.balatoni@gmail.com>
2016-10-09 14:30:41 +0200

    Change: move constant global variables into a separate settings file (module)

--- dodger.py
+++ dodger.py
@@ -1,17 +1,6 @@
 import pygame, random, sys
 from pygame.locals import *
-
-WINDOWWIDTH = 600
-WINDOWHEIGHT = 600
-TEXTCOLOR = (255, 255, 255)
-BACKGROUNDCOLOR = (0, 0, 0)
-FPS = 40
-BADDIEMINSIZE = 10
-BADDIEMAXSIZE = 40
-BADDIEMINSPEED = 1
-BADDIEMAXSPEED = 8
-ADDNEWBADDIERATE = 6
-PLAYERMOVERATE = 5
+from settings import * # import constant variables from separate settings file
 
 def terminate():
     pygame.quit()
